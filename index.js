c834046a9dde4edb38ab0fdf6ce257e47f7fd1e1
Silviu Iosif <6332838@rts-365.dk>
2020-10-22 14:29:01 +0200

    updated JS

---
+++ index.js
@@ -0,0 +1,14 @@
+// spread syntax 
+function createElement(tag, ...attributes){
+    if(!tag){
+        console.error("tag is mandatory");
+        return;    
+    }
+    var element = document.createElement(tag);
+    attributes.forEach(attribute => {
+        element.setAttribute(attribute[0], attribute[1]);
+    })
+    
+    return element;   
+}
+export default createElement;
